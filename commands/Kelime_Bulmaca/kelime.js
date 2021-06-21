ac39efc5727e246268695af0821c08b6db72d910
Glitch (wholesale-legendary-chicken) <none>
2021-06-21 18:17:33 +0000

    🚅📰 Checkpoint ./commands/Kelime_Bulmaca/kelime.js:20402966/84

--- commands/Kelime_Bulmaca/kelime.js
+++ commands/Kelime_Bulmaca/kelime.js
@@ -249,8 +249,8 @@ module.exports = {
             break;
           case 'istatistik':
             const istatistikozel = new Discord.MessageEmbed()
-  .setColor(0x36393F)
-.setDescription(`${client.user.username}`)
+  .setColor("#FFCC00")
+//.setDescription(`${client.user.username}`)
 .addField(`Bot Sahibi`, `<@535033289346514964>`, )
 .addField("Sunucu Sayısı ", `${client.guilds.cache.size.toLocaleString()}`, true)
 .addField("Toplam Kullanıcı Sayısı ", `${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}`, true)
@@ -258,7 +258,6 @@ module.exports = {
 .addField("Discord.js Sürümü ", `${Discord.version}`, true)  
  .addField(`Destek Sunucum`, `[Tıkla]( https://discord.gg/yr4xpYJvvp)`, true)
 .addField(`Botu Davet Et`, `[Tıkla](https://discord.com/oauth2/authorize?client_id=856238953296691200&scope=bot&permissions=3664960)`, true)
-//.addField(`Websitem`, `[http://enesozdemir.site/]( )`, true)
   .setImage("https://cdn.discordapp.com/attachments/802906619234222081/856207093413183508/tan-yellow-pp.jpg")
   .setFooter(`${message.author.tag} Tarafından İstendi.`, message.author.avatarURL)
   return message.channel.send(istatistikozel);
